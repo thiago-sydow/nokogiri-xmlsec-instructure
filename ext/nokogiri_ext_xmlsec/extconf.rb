cc9a1630b938ed36d1ecedd2e2e2c35ba3905dd1
Albert J. Wong <omb.awong@gmail.com>
2014-10-09 12:30:22 -0700

    Ensure -L from pkg-config paths are also in build.

--- ext/nokogiri_ext_xmlsec/extconf.rb
+++ ext/nokogiri_ext_xmlsec/extconf.rb
@@ -14,8 +14,9 @@ if $CFLAGS =~ /\-DXMLSEC_CRYPTO=\\\\\\"openssl\\\\\\"/
 end
 
 $CFLAGS += " " << `pkg-config --cflags xmlsec1-openssl`.strip
-$CXXFLAGS = " " << `pkg-config --cflags xmlsec1-openssl`.strip
-$libs = " " << `pkg-config --libs-only-l xmlsec1-openssl`.strip
+$CXXFLAGS += " $CFLAGS"
+$LDFLAGS += " " << `pkg-config --libs-only-L xmlsec1-openssl`.strip
+$libs += " " << `pkg-config --libs-only-l xmlsec1-openssl`.strip
 
 have_library 'xmlsec1-openssl'
 create_makefile('nokogiri_ext_xmlsec')
